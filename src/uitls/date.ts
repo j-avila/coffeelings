c3020ce51fc0bfbc5d3a8ad3b7896630d77469e3
j-avila <jose.leon.avila@gmail.com>
2024-03-03 17:36:45 -0300

    feat(auth): :sparkles: auth validation
    created an provider and the sign in & out functions and protected views

--- src/uitls/date.ts
+++
@@ -1,9 +0,0 @@
-import dayjs from 'dayjs';
-
-export const getNowObj = () => {
-  return {
-    year: dayjs().get('year'),
-    month: dayjs().format('MMM'),
-    day: dayjs().get('date'),
-  };
-};
